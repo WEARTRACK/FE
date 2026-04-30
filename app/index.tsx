10fa8ec6883c03c6a6a6d881c1099b481f8f753f
jeje <kcj76wj7qv@privaterelay.appleid.com>
2026-04-30 20:46:54 +0900

    fix: 가입 성공 및 닉네임 설정 화면 일관성 수정

--- app/index.tsx
+++ app/index.tsx
@@ -3,5 +3,6 @@ import { Redirect } from "expo-router";
 
 export default function RootIndexRoute() {
   // return <Redirect href="/home" />;
+  // return <Redirect href="auth/sign-up" />;
   return <Redirect href="/auth/sign-up-success" />;
 }
