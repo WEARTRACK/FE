6ff2c00a59c86a244db531658bc8f387212bcbce
jangseoyeong <suhyoung030327@naver.com>
2026-05-11 14:07:18 +0900

    index 파일 정리

--- app/index.tsx
+++ app/index.tsx
@@ -2,11 +2,6 @@ import { SplashScreen } from "@/features/entry/screens/SplashScreen";
 // import { Redirect } from "expo-router";
 
 export default function RootIndexRoute() {
-<<<<<<< feature/clothes-filter-api
-  // return <SplashScreen />;
-  return <Redirect href="/auth/sign-in" />;
-=======
   return <SplashScreen />;
   // return <Redirect href="/closet" />;
->>>>>>> develop
 }
